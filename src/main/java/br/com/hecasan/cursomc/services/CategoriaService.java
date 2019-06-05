844b38dd51f1a06ef83bb71e06118506d2e22b2f
hecasan <hetecaetano@gmail.com>
2019-06-05 11:37:13 -0300

    Criando repository e service para Categoria

--- src/main/java/br/com/hecasan/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/hecasan/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package br.com.hecasan.cursomc.services;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.hecasan.cursomc.domain.Categoria;
@@ -10,6 +11,7 @@ import br.com.hecasan.cursomc.repositories.CategoriaRepository;
 @Service
 public class CategoriaService {
 	
+	@Autowired
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
